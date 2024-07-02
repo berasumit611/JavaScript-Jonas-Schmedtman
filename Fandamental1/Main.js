38ab749fb3c888bdf2a83cfa534f8657f0f4ec25
Sumit Bera <berasumit611@gmail.com>
2024-07-02 20:31:17 +0530

    Update 2

---
+++ Fandamental1/Main.js
@@ -0,0 +1,23 @@
+"use strict";
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country, continent, population;
+country = "India";
+population = "1.6 billion";
+continent = "Asia";
+
+console.log(
+  `my country ${country} its population ${population} and its continent ${continent}`
+);
+
+console.log("-----------------------");
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// Log the types of isIsland, population, country and language to the console.
+let island = false;
+let language = "hindi";
+console.log(typeof island); //boolean
+console.log(typeof language);
+console.log("-----------------------");
